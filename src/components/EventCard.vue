1047bd96e6c4174ec8a36749992fea0e33a62735
Eduard <terriblegoodday@icloud.com>
2019-11-07 19:30:47 +1000

    Update EventCard.vue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="event">
-    <div v-if="event != null" class="container">
+  <div class='event'>
+    <div v-if='event != null || event != undefined' class='container' @click='eventClicked'>
       <h2><span>{{event.place}}</span> {{event.description}}</h2>
-      <h3>{{event.teacher.fullName}}</h3>
-      <h3>Лабораторные занятия</h3>
+      <h3 class='teacher'>{{event.teacher.fullName}}</h3>
+      <h3>{{event.formattedType}}</h3>
     </div>
   </div>
 </template>
 
 <script>
+import EventBus from '../eventbus'
+
 export default {
-  name: "EventCard",
-  props: ['event']
+  name: 'EventCard',
+  props: ['event'],
+  methods: {
+    eventClicked: function () {
+      EventBus.$emit('showModalWith', this.event)
+    }
+  }
 }
 </script>
